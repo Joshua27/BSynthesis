14d5a3ed29d2abc8f1c2964af8bb65e46c26fa8f
schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-06-16 15:23:58 +0200

    use logger from org.slf4j

--- src/main/java/de/hhu/stups/bsynthesis/ui/Loader.java
+++ src/main/java/de/hhu/stups/bsynthesis/ui/Loader.java
@@ -2,9 +2,10 @@ package de.hhu.stups.bsynthesis.ui;
 
 import javafx.fxml.FXMLLoader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public interface Loader {
 
@@ -21,8 +22,8 @@ public interface Loader {
     try {
       loader.load();
     } catch (final IOException exception) {
-      final Logger logger = Logger.getLogger(root.getClass().getSimpleName());
-      logger.log(Level.SEVERE, "Loading fxml for " + root.toString() + " failed.", exception);
+      final Logger logger = LoggerFactory.getLogger(root.getClass());
+      logger.error("Loading fxml for " + root.toString() + " failed.", exception);
     }
   }
 
