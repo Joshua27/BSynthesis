1472edffc026a56c0fcb1329f0e4ca0cea0be0d1
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-06-27 13:46:10 +0200

    add wrapper #3

---
+++ src/main/java/de/hhu/stups/bsynthesis/prob/DistinguishingExample.java
@@ -0,0 +1,47 @@
+package de.hhu.stups.bsynthesis.prob;
+
+import com.google.common.base.Joiner;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * A distinguishing example containing input and output states represented as lists of {@link
+ * VarValueTuple}.
+ */
+public final class DistinguishingExample {
+
+  private List<VarValueTuple> inputTuples;
+  private List<VarValueTuple> outputTuples;
+
+  DistinguishingExample(final List<VarValueTuple> inputTuples,
+                        final List<VarValueTuple> outputTuples) {
+    this.inputTuples = inputTuples;
+    this.outputTuples = outputTuples;
+  }
+
+  public List<VarValueTuple> getInputTuples() {
+    return inputTuples;
+  }
+
+  public List<VarValueTuple> getOutputTuples() {
+    return outputTuples;
+  }
+
+  public String getInputStateEquality() {
+    return getStateEquality(inputTuples);
+  }
+
+  public String getOutputStateEquality() {
+    return getStateEquality(outputTuples);
+  }
+
+  /**
+   * Return a string of equalities between variable and value.
+   */
+  private String getStateEquality(final List<VarValueTuple> tuples) {
+    return Joiner.on(" & ").join(tuples.stream().map(varValueTuple ->
+        varValueTuple.getVar() + "=" + varValueTuple.getValue().replace("'", ""))
+        .collect(Collectors.toList()));
+  }
+}
---
+++ src/main/java/de/hhu/stups/bsynthesis/prob/VarValueTuple.java
@@ -0,0 +1,20 @@
+package de.hhu.stups.bsynthesis.prob;
+
+public final class VarValueTuple {
+  private final String var;
+  private final String value;
+
+  VarValueTuple(final String var,
+                final String value) {
+    this.var = var;
+    this.value = value;
+  }
+
+  public String getVar() {
+    return var;
+  }
+
+  public String getValue() {
+    return value;
+  }
+}
