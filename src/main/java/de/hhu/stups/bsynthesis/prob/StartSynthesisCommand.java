7bfe24ff37bf36816e30d8aef8e1cd82c8bb787b
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-06-27 13:46:47 +0200

    extend synthesis command, handle distinguishing interactions, closes #3

--- src/main/java/de/hhu/stups/bsynthesis/prob/StartSynthesisCommand.java
+++ src/main/java/de/hhu/stups/bsynthesis/prob/StartSynthesisCommand.java
@@ -6,37 +6,44 @@ import de.hhu.stups.bsynthesis.ui.components.nodes.BasicNode;
 import de.hhu.stups.bsynthesis.ui.components.nodes.StateNode;
 import de.hhu.stups.bsynthesis.ui.components.nodes.TransitionNode;
 import de.prob.animator.command.AbstractCommand;
+import de.prob.parser.BindingGenerator;
 import de.prob.parser.ISimplifiedROMap;
 import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.CompoundPrologTerm;
 import de.prob.prolog.term.PrologTerm;
 import de.prob.statespace.State;
 
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StartSynthesisCommand extends AbstractCommand {
 
   private static final String PROLOG_COMMAND_NAME = "start_synthesis";
-  private static final String SYNTHESIS_SUCCEEDED = "Succeeded";
+  private static final String DISTINGUISHING_EXAMPLE = "Distinguishing";
   private static final String MODIFIED_MACHINE = "NewMachine";
 
   private final Logger logger = LoggerFactory.getLogger(getClass());
   private final String currentOperation;
-  private final String synthesisType;
+  private final SynthesisType synthesisType;
   private final Set<InputOutputExample> positiveExamples;
   private final Set<InputOutputExample> negativeExamples;
   private final BLibrary selectedLibraryComponents;
   private final BooleanProperty synthesisSucceededProperty;
   private final StringProperty modifiedMachineCodeProperty;
+  private final ObjectProperty<DistinguishingExample> distinguishingExampleProperty;
 
   /**
    * Start the synthesis workflow by calling the prolog backend.
@@ -47,12 +54,50 @@ public class StartSynthesisCommand extends AbstractCommand {
                                final List<BasicNode> positiveExamples,
                                final List<BasicNode> negativeExamples) {
     this.currentOperation = currentOperation;
-    this.synthesisType = synthesisType.toString().toLowerCase();
+    this.synthesisType = synthesisType;
     this.positiveExamples = getInputOutputExamples(positiveExamples);
     this.negativeExamples = getInputOutputExamples(negativeExamples);
     this.selectedLibraryComponents = selectedLibraryComponents;
     synthesisSucceededProperty = new SimpleBooleanProperty(false);
-    modifiedMachineCodeProperty = new SimpleStringProperty();
+    modifiedMachineCodeProperty = new SimpleStringProperty("");
+    distinguishingExampleProperty = new SimpleObjectProperty<>();
+  }
+
+  @Override
+  public void writeCommand(final IPrologTermOutput pto) {
+    pto.openTerm(PROLOG_COMMAND_NAME)
+        .printAtom(selectedLibraryComponents.considerIfStatementsProperty().get()
+            ? "yes" : "no")
+        .printAtom(currentOperation)
+        .printAtom(synthesisType.toString().toLowerCase());
+    printPrologListFromExamples(pto, positiveExamples);
+    printPrologListFromExamples(pto, negativeExamples);
+    pto.printVariable(MODIFIED_MACHINE).printVariable(DISTINGUISHING_EXAMPLE).closeTerm();
+    System.out.println(pto.toString());
+    logger.info("Start synthesis prolog backend by calling prob2_interface: {}", pto);
+  }
+
+  private void printPrologListFromExamples(final IPrologTermOutput pto,
+                                           final Set<InputOutputExample> examples) {
+    pto.openList();
+    examples.forEach(example -> {
+      pto.openTerm(",");
+      example.printInputOutputStateToPrologTerm(pto);
+      pto.closeTerm();
+    });
+    pto.closeList();
+  }
+
+  @Override
+  public void processResult(final ISimplifiedROMap<String, PrologTerm> bindings) {
+    final String newMachineCode = bindings.get(MODIFIED_MACHINE).getFunctor();
+    if ("none".equals(newMachineCode)) {
+      logger.info("Distinguishing example: {}", bindings.get(DISTINGUISHING_EXAMPLE));
+      setDistinguishingExample(bindings.get(DISTINGUISHING_EXAMPLE));
+      return;
+    }
+    synthesisSucceededProperty.set(true);
+    modifiedMachineCodeProperty.set(newMachineCode);
   }
 
   private Set<InputOutputExample> getInputOutputExamples(final List<BasicNode> examples) {
@@ -81,11 +126,17 @@ public class StartSynthesisCommand extends AbstractCommand {
                             final StateNode stateNode) {
     final State state = stateNode.getState();
     if (stateNode.isValidatedPositive()) {
-      // set the positive state to be an output examples and compute the corresponding input state
-      final State predecessorState = stateNode.getPredecessor();
-      if (predecessorState != null) {
+      // set the positive state to be an input examples and compute the corresponding output state
+      final StateNode successorState = stateNode.getSuccessorFromTrace();
+      if (successorState != null) {
+        inputOutputExamples.add(new InputOutputExample(
+            new ExampleState(state),
+            new ExampleState(successorState.getState())));
+      } else {
+        // no successor, just add the same node since we synthesize guard/invariant and
+        // split transitions
         inputOutputExamples.add(
-            new InputOutputExample(new ExampleState(predecessorState), new ExampleState(state)));
+            new InputOutputExample(new ExampleState(state), new ExampleState(state)));
       }
       return;
     }
@@ -97,36 +148,39 @@ public class StartSynthesisCommand extends AbstractCommand {
         new ExampleState(state)));
   }
 
-  @Override
-  public void writeCommand(final IPrologTermOutput pto) {
-    pto.openTerm(PROLOG_COMMAND_NAME)
-        .printAtom(selectedLibraryComponents.considerIfStatementsProperty().get()
-            ? "yes" : "no")
-        .printAtom(currentOperation)
-        .printAtom(synthesisType);
-    printPrologListFromExamples(pto, positiveExamples);
-    printPrologListFromExamples(pto, negativeExamples);
-    pto.printVariable(SYNTHESIS_SUCCEEDED).printVariable(MODIFIED_MACHINE).closeTerm();
-    System.out.println(pto.toString());
-    logger.info("Start synthesis prolog backend by calling prob2_interface:{}", pto);
+  private BasicNode setDistinguishingExample(final PrologTerm prologTerm) {
+    final String resultFunctor = prologTerm.getFunctor();
+    switch (resultFunctor) {
+      case "state":
+        final List<VarValueTuple> stateList = BindingGenerator.getList(prologTerm.getArgument(1))
+            .stream().map(this::getVarValueTupleFromProlog).collect(Collectors.toList());
+        distinguishingExampleProperty.set(
+            new DistinguishingExample(stateList, Collections.emptyList()));
+        break;
+      case "transition":
+        final List<VarValueTuple> inputStateList =
+            BindingGenerator.getList(prologTerm.getArgument(1))
+                .stream().map(this::getVarValueTupleFromProlog).collect(Collectors.toList());
+        final List<VarValueTuple> outputStateList =
+            BindingGenerator.getList(prologTerm.getArgument(2))
+                .stream().map(this::getVarValueTupleFromProlog).collect(Collectors.toList());
+        distinguishingExampleProperty.set(
+            new DistinguishingExample(inputStateList, outputStateList));
+        break;
+      default:
+        throw new AssertionError("Unexpected result of synthesis command.");
+    }
+    return null;
   }
 
-  private void printPrologListFromExamples(final IPrologTermOutput pto,
-                                           final Set<InputOutputExample> examples) {
-    pto.openList();
-    examples.forEach(example -> {
-      pto.openTerm(",");
-      example.printInputOutputStateToPrologTerm(pto);
-      pto.closeTerm();
-    });
-    pto.closeList();
+  private VarValueTuple getVarValueTupleFromProlog(final PrologTerm prologTerm) {
+    final CompoundPrologTerm compoundPrologTerm = BindingGenerator.getCompoundTerm(prologTerm, 2);
+    return new VarValueTuple(compoundPrologTerm.getArgument(1).toString(),
+        compoundPrologTerm.getArgument(2).toString());
   }
 
-  @Override
-  public void processResult(final ISimplifiedROMap<String, PrologTerm> bindings) {
-    logger.info("Synthesis succeeded? {}", bindings.get(SYNTHESIS_SUCCEEDED));
-    synthesisSucceededProperty.set("yes".equals(bindings.get(SYNTHESIS_SUCCEEDED).getFunctor()));
-    modifiedMachineCodeProperty.set(bindings.get(MODIFIED_MACHINE).getFunctor());
+  public ObjectProperty<DistinguishingExample> distinguishingExampleProperty() {
+    return distinguishingExampleProperty;
   }
 
   public BooleanProperty synthesisSucceededProperty() {
