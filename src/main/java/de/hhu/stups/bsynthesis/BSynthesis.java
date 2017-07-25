4af8f664df783a3daf96f952e0227e5a9dd61081
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-07-25 21:06:46 +0200

    exit system on stop

--- src/main/java/de/hhu/stups/bsynthesis/BSynthesis.java
+++ src/main/java/de/hhu/stups/bsynthesis/BSynthesis.java
@@ -42,5 +42,6 @@ public class BSynthesis extends Application {
   public void stop() {
     injector.getInstance(ProBInstanceProvider.class).shutdownAll();
     Platform.exit();
+    System.exit(0);
   }
 }
