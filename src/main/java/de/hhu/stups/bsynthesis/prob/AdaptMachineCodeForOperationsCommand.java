47942f50cba1768adaf4e48613779172c4003f41
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2017-09-23 00:10:19 +0200

    add ProB2 command to apply a set of synthesized operations to the machine code

---
+++ src/main/java/de/hhu/stups/bsynthesis/prob/AdaptMachineCodeForOperationsCommand.java
@@ -0,0 +1,40 @@
+package de.hhu.stups.bsynthesis.prob;
+
+import de.prob.animator.command.AbstractCommand;
+import de.prob.parser.ISimplifiedROMap;
+import de.prob.prolog.output.IPrologTermOutput;
+import de.prob.prolog.term.CompoundPrologTerm;
+import de.prob.prolog.term.PrologTerm;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.util.Set;
+
+public class AdaptMachineCodeForOperationsCommand extends AbstractCommand {
+
+  private static final String PROLOG_COMMAND_NAME = "adapt_machine_code_for_operations";
+  private static final String MACHINE_CODE = "NewMachineCode";
+
+  private final Set<CompoundPrologTerm> operations;
+  private StringProperty machineCodeProperty = new SimpleStringProperty();
+
+  AdaptMachineCodeForOperationsCommand(final Set<CompoundPrologTerm> operations) {
+    this.operations = operations;
+  }
+
+  @Override
+  public void writeCommand(final IPrologTermOutput pto) {
+    pto.openTerm(PROLOG_COMMAND_NAME).openList();
+    operations.forEach(compoundPrologTerm -> pto.printTerm(compoundPrologTerm.getArgument(2)));
+    pto.closeList().printVariable(MACHINE_CODE).closeTerm();
+  }
+
+  @Override
+  public void processResult(final ISimplifiedROMap<String, PrologTerm> bindings) {
+    machineCodeProperty.set(bindings.get(MACHINE_CODE).getFunctor());
+  }
+
+  StringProperty machineCodeProperty() {
+    return machineCodeProperty;
+  }
+}
