66231b15bc9eb1dbf1659247db6ed4f7ed50c9c8
Joshua Schmidt <joshua.schmidt@uni-duesseldorf.de>
2018-10-26 17:31:41 +0200

    add gradle task to build fat jar

--- src/main/java/de/hhu/stups/bsynthesis/BSynthesis.java
+++ src/main/java/de/hhu/stups/bsynthesis/BSynthesis.java
@@ -1,59 +1,61 @@
-package de.hhu.stups.bsynthesis;
+package de.hhu.stups.bsynthesis
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.google.inject.Guice
+import com.google.inject.Injector
 
-import de.hhu.stups.bsynthesis.injector.BSynthesisModule;
-import de.hhu.stups.bsynthesis.services.ApplicationEvent;
-import de.hhu.stups.bsynthesis.services.ApplicationEventType;
-import de.hhu.stups.bsynthesis.services.UiService;
-import de.hhu.stups.bsynthesis.ui.controller.SynthesisMain;
-import de.prob.cli.ProBInstanceProvider;
+import de.hhu.stups.bsynthesis.injector.BSynthesisModule
+import de.hhu.stups.bsynthesis.services.ApplicationEvent
+import de.hhu.stups.bsynthesis.services.ApplicationEventType
+import de.hhu.stups.bsynthesis.services.UiService
+import de.hhu.stups.bsynthesis.ui.controller.SynthesisMain
+import de.prob.cli.ProBInstanceProvider
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.application.Application
+import javafx.application.Platform
+import javafx.scene.Scene
+import javafx.stage.Stage
 
-public class BSynthesis extends Application {
+class BSynthesis : Application() {
 
-  private Injector injector;
-  private UiService uiService;
+    private var injector: Injector? = null
+    private var uiService: UiService? = null
 
-  public static void main(final String... args) {
-    launch(args);
-  }
+    override fun start(stage: Stage) {
+        System.setProperty("logback.configurationFile", "config/logging.xml")
 
-  @Override
-  public void start(final Stage stage) {
-    System.setProperty("logback.configurationFile", "config/logging.xml");
+        injector = Guice.createInjector(BSynthesisModule())
 
-    injector = Guice.createInjector(new BSynthesisModule());
+        uiService = injector!!.getInstance<UiService>(UiService::class.java)
+        val root = injector!!.getInstance<SynthesisMain>(SynthesisMain::class.java)
+        val mainScene = Scene(root, 1024.0, 768.0)
+        root.getStylesheets().add("main.css")
+        root.setStage(stage)
 
-    uiService = injector.getInstance(UiService.class);
-    final SynthesisMain root = injector.getInstance(SynthesisMain.class);
-    final Scene mainScene = new Scene(root, 1024, 768);
-    root.getStylesheets().add("main.css");
-    root.setStage(stage);
+        stage.title = "BSynthesis"
+        stage.scene = mainScene
+        stage.setOnCloseRequest { e -> Platform.exit() }
 
-    stage.setTitle("BSynthesis");
-    stage.setScene(mainScene);
-    stage.setOnCloseRequest(e -> Platform.exit());
+        stage.show()
+    }
 
-    stage.show();
-  }
+    /**
+     * Shut down all injector instances and send [ApplicationEventType.CLOSE_APP] to the
+     * [UiService.applicationEventStream] to shut down all executor services etc.
+     */
+    override fun stop() {
+        injector!!.getInstance<ProBInstanceProvider>(ProBInstanceProvider::class.java).shutdownAll()
+        uiService!!.applicationEventStream().push(
+                ApplicationEvent(ApplicationEventType.CLOSE_APP))
+        Platform.exit()
+        // TODO: fix this, the model checker seems to be not closed properly
+        System.exit(0)
+    }
 
-  /**
-   * Shut down all injector instances and send {@link ApplicationEventType#CLOSE_APP} to the
-   * {@link UiService#applicationEventStream} to shut down all executor services etc.
-   */
-  @Override
-  public void stop() {
-    injector.getInstance(ProBInstanceProvider.class).shutdownAll();
-    uiService.applicationEventStream().push(
-        new ApplicationEvent(ApplicationEventType.CLOSE_APP));
-    Platform.exit();
-    // TODO: fix this, the model checker seems to be not closed properly
-    System.exit(0);
-  }
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(*args)
+        }
+    }
 }
